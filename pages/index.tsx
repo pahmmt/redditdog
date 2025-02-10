c5a18c5054e89438d62e4079451dafe82c0301a2
PMT <pmt9819@gmail.com>
2025-02-10 13:54:50 +0700

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -253,7 +253,7 @@ export default function Home() {
                     <li>Community growth</li>
                     <li>Spread the memes</li>
                     <li>Onboard redditors to crypto</li>
-                    <li>Flip Reddit stock</li>
+                    <li>Flip the Reddit stock</li>
                   </ol>
                 </div>
               </div>
